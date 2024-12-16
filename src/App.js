0191d9f856d1ac70dda2b95959a61e59bee26d89
elhalj <wilsonikedakoffi7@gmail.com>
2024-12-16 13:47:39 +0000

    exo resolu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      personne: {
+        fullName: 'Wilson ikeda',
+        bio: 'Développeur web',
+        imgSrc: 'https://img.freepik.com/photos-gratuite/portrait-programmeur-afro-americain-concentre-portant-ecouteurs-sans-fil-travaillant-regardant-ecran-ordinateur-tout-tapant-developpeur-logiciels-ecrivant-du-code-dans-bureau-agence-informatique_482257-43992.jpg',
+        profession: 'Ingénieur logiciel'
+      },
+      montre: false,
+      tempsEcoule: 0
+    };
+    this.affichage = this.affichage.bind(this);
+  }
+  ajoutInterval() {
+    this.ajoutInterval(() => {
+      this.setState(precTemps => ({tempsEcoule: precTemps.tempsEcoule ++}))
+    },1000);
+  }
+
+  fermerInterval() {
+    clearInterval(this.ajoutInterval);
+  }
+  
+  affichage() {
+    this.setState(precState => ({montre: !precState.montre}));
+  }
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.personne;
+    const { montre, tempsEcoule } = this.state;
+
+    return (
+      <>
+        <div>
+          <h1>PROFIL PERSONNE</h1>
+          <button onClick={this.affichage}>
+            {montre ? 'Afficher Profil' : 'Cacher Profil'}
+          </button>
+          {montre && (
+            <div>
+              <h1>{fullName}</h1>
+              <p>{bio}</p>
+              <img src={imgSrc} alt={fullName} />
+              <p>{profession}</p>
+            </div>
+          )}
+          <p>Temps ecoule {tempsEcoule} secondes</p>
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
